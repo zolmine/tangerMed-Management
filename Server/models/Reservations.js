fd39f547d9e619418790476a7f5bba8f771ba837
zolmine <68522174+zolmine@users.noreply.github.com>
2022-02-11 09:04:26 +0000

    adding some Schemas to the models in the server Folder

---
+++ Server/models/Reservations.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const Reservations = new Schema(
+  {
+    reference: String,
+    checkIdDate: String,
+    checkOutDate: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Reservations", Reservations);
