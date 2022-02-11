e09574a5dcec1eb329639494ca05011e735b83b1
zolmine <68522174+zolmine@users.noreply.github.com>
2022-02-11 10:18:49 +0000

    setting Up the controllers nd the routers

--- Server/models/Reservations.js
+++ Server/models/Reservations.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+// const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
 const Reservations = new Schema(
   {
---
+++ Server/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express();
+
+
+// router.use("/customers/", customer)
+// router.use("/rooms/", rooms)
+// router.use("/reservation", reservation)
+
+// router.get("/", availableRooms) //return available rooms 
+// router.get("/booked", bookedRooms) //return booked rooms
+// router.post("/booking", bookingRoom)
+
+
+module.exports = router
